08b34b91c56c0dc75b8bd637589251475f7badd6
ahmedmellit <119601555+ahmedmellit@users.noreply.github.com>
2023-06-16 18:33:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,31 +4,31 @@ import pandas as pd
 import numpy as np
 
 def dijkstra(graph, start, end):
-    distances = {node: float('inf') for node in graph}
-    distances[start] = 0
-    visited = set()
+    distances = {node: float('inf') for node in graph}  # Initialisation des distances à l'infini pour chaque nœud
+    distances[start] = 0  # La distance du nœud de départ est définie à 0
+    visited = set()  # Ensemble des nœuds visités
 
-    pq = [(0, start)]
+    pq = [(0, start)]  # Initialisation de la file de priorité avec le nœud de départ
 
     while pq:
-        current_distance, current_node = heappop(pq)
-        visited.add(current_node)
+        current_distance, current_node = heappop(pq)  # Retirer le nœud avec la plus petite distance actuelle de la file de priorité
+        visited.add(current_node)  # Ajouter le nœud visité à l'ensemble des nœuds visités
 
-        if current_node == end:
+        if current_node == end:  # Si le nœud actuel est le nœud d'arrivée, on a trouvé le chemin optimal
             break
 
         for neighbor, weight in graph[current_node].items():
-            if neighbor not in visited:
+            if neighbor not in visited:  # Vérifier si le voisin n'a pas été visité
                 distance = current_distance + weight
 
-                if distance < distances[neighbor]:
-                    distances[neighbor] = distance
-                    heappush(pq, (distance, neighbor))
+                if distance < distances[neighbor]:  # Vérifier si la nouvelle distance est plus courte que la distance précédemment enregistrée
+                    distances[neighbor] = distance  # Mettre à jour la distance du voisin
+                    heappush(pq, (distance, neighbor))  # Ajouter le voisin à la file de priorité avec sa nouvelle distance
 
     return distances
 
 def main():
-    st.title("Wheelchair-Friendly Navigation App")
+    st.title("Application de navigation accessible aux fauteuils roulants")
 
     min_lat, max_lat = 34.00, 34.10
     min_lon, max_lon = -5.05, -4.95
@@ -41,7 +41,7 @@ def main():
 
     st.map(df)
 
-    st.write("Enter the starting and ending nodes to find the optimal wheelchair-friendly route.")
+    st.write("Entrez les nœuds de départ et d'arrivée pour trouver le chemin optimal accessible aux fauteuils roulants.")
 
     graph = {
     'Ain Nokbi Park': {'Al-Qarawiyyin Mosque': 2, 'Jardin Jnan Sbil': 3, 'Batha Museum': 4},
@@ -79,8 +79,8 @@ def main():
 
     nodes = list(graph.keys())
 
-    start_node = st.selectbox("Select the starting node:", nodes)
-    end_node = st.selectbox("Select the ending node:", nodes)
+    start_node = st.selectbox("Sélectionnez le nœud de départ :", nodes)
+    end_node = st.selectbox("Sélectionnez le nœud de destination :", nodes)
 
     distances = dijkstra(graph, start_node, end_node)
     optimal_path = []
@@ -92,10 +92,10 @@ def main():
     optimal_path.append(start_node)
     optimal_path.reverse()
 
-    st.write("Optimal Path:")
+    st.write("Chemin optimal :")
     st.write(optimal_path)
 
-    st.write("Total Distance:", distances[end_node])
+    st.write("Distance totale :", distances[end_node])
 
 if __name__ == "__main__":
     main()
